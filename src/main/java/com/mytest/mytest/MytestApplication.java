1e50e965e8d9c014f24cc7241baa785553e564d8
hosseinDalir <dalirianh70@gmail.com>
2023-08-02 15:18:17 +0330

    second  commit

--- src/main/java/com/mytest/mytest/MytestApplication.java
+++ src/main/java/com/mytest/mytest/MytestApplication.java
@@ -13,6 +13,8 @@ public class MytestApplication {
         System.out.println("print");
         System.out.println("print");
         System.out.println("print");
+        System.out.println("print");
+
 
 
 
